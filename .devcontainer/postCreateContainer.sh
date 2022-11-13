7ad2da8872818e7029196fbd7e66abf5814f8c2c
Tao Hansen <59834693+worldofgeese@users.noreply.github.com>
2022-11-13 21:29:44 +0000

    Copy Kubernetes config during devcontainer setup

--- .devcontainer/postCreateContainer.sh
+++ .devcontainer/postCreateContainer.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 poetry install
 poetry export -f requirements.txt --output requirements.txt
+
+mkdir -p $HOME/.kube
+cp /usr/local/share/kube-localhost/config $HOME/.kube/config
