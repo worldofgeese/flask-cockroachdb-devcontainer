62eed96101251d1e3aa586e9813f133894856245
Tao <59834693+worldofgeese@users.noreply.github.com>
2022-12-23 20:00:50 +0100

    Add GCP auth
    Mounts in gcloud configuration to allow GCP auth inside the devcontainer.

--- .devcontainer/postCreateContainer.sh
+++ .devcontainer/postCreateContainer.sh
@@ -4,3 +4,6 @@ poetry export -f requirements.txt --output requirements.txt
 
 mkdir -p $HOME/.kube
 cp /usr/local/share/kube-localhost/config $HOME/.kube/config
+
+mkdir -p $HOME/.config/gcloud
+cp -r /usr/local/share/gcloud-localhost/* $HOME/.config/gcloud/
