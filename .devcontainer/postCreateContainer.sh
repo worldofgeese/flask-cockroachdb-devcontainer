e5655baae9a6ec3f5f05f86d372e7f2c02efef1b
Tao <59834693+worldofgeese@users.noreply.github.com>
2022-12-27 10:25:25 +0100

    PDM for Python package mgmt
    Garden's Pulumi integration doesn't detect virtual environments and while I could have used Python's native venvs Pulumi supports, I'd rather have no virtual environments using PDM's support for PEP-582 so there's fewer pieces to break.

--- .devcontainer/postCreateContainer.sh
+++ .devcontainer/postCreateContainer.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
-poetry install
-poetry export -f requirements.txt --output requirements.txt
+pdm install
+pdm export -o requirements.txt
 
 mkdir -p $HOME/.kube
 cp /usr/local/share/kube-localhost/config $HOME/.kube/config
 
 mkdir -p $HOME/.config/gcloud
 cp -r /usr/local/share/gcloud-localhost/* $HOME/.config/gcloud/
+
+mkdir -p $HOME/.pulumi
+cp -r /usr/local/share/pulumi-localhost/* $HOME/.pulumi/
